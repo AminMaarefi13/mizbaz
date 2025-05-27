76bf0fa6c22693add73a0b6007e58afd49d70f47
golden <amdexec@gmail.com>
2025-05-27 20:16:31 +0330

    redis url

--- Server/utils/redisClient.js
+++ Server/utils/redisClient.js
@@ -1,9 +1,13 @@
 const { createClient } = require("redis");
 
-const redisClient =
+const redisUrl =
   process.env.NODE_ENV === "production"
     ? process.env.REDIS
     : "redis://localhost:6379";
+
+const redisClient = createClient({
+  url: redisUrl,
+});
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
