75c130e6193d0ce70826bf83b018d560d0c8e6c6
golden <amdexec@gmail.com>
2025-05-27 20:26:34 +0330

    server url added to front

--- Client/src/network/socket.js
+++ Client/src/network/socket.js
@@ -1,10 +1,22 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3001", {
+
+const SOCKET_URL =
+  import.meta.env.VITE_MODE === "production"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:3001";
+
+export const socket = io(SOCKET_URL, {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
 });
+// export const socket = io("http://localhost:3001", {
+//   autoConnect: false,
+//   reconnection: true,
+//   reconnectionAttempts: 5,
+//   reconnectionDelay: 1000,
+// });
 
 // توکن یا playerId رو از localStorage بخون
 const playerId = localStorage.getItem("playerId");
