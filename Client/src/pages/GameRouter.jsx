da30fbec30de658ebe2c4fc05902b9a28bd28a7d
golden <amdexec@gmail.com>
2025-06-06 11:23:14 +0330

    Reloading on gamePage now redirects to lobbyPage mapPage only renders for games with maps

--- Client/src/pages/GameRouter.jsx
+++ Client/src/pages/GameRouter.jsx
@@ -1,3 +1,4 @@
+import { Navigate } from "react-router-dom";
 import { useGameContext } from "../context/GameContext";
 import FeedTheKrakenPage from "./FeedTheKrakenPage";
 import MineSweeperPage from "./MineSweeperPage";
@@ -5,8 +6,10 @@ import MineSweeperPage from "./MineSweeperPage";
 export default function GameRouter() {
   const { gameState } = useGameContext();
   console.log("GameRouter - Current Game State:", gameState);
-  if (!gameState || !gameState.type) {
-    return <div>در حال بارگذاری اطلاعات بازی...</div>;
+
+  // اگر هنوز نوع بازی تعیین نشده، کاربر را به لابی بفرست
+  if (!gameState?.type) {
+    return <Navigate to="/lobby" replace />; // replace == بدون اضافه‌شدن به history
   }
 
   switch (gameState.type) {
@@ -15,6 +18,31 @@ export default function GameRouter() {
     case "mineSweeper":
       return <MineSweeperPage />;
     default:
-      return <div>نوع بازی پشتیبانی نمی‌شود.</div>;
+      // نوع نامعتبر → برگشت به لابی
+      return <Navigate to="/lobby" replace />;
   }
 }
+
+// import { useGameContext } from "../context/GameContext";
+// import FeedTheKrakenPage from "./FeedTheKrakenPage";
+// import MineSweeperPage from "./MineSweeperPage";
+// import { useNavigate } from "react-router-dom";
+
+// export default function GameRouter() {
+//   const { gameState } = useGameContext();
+//   const navigate = useNavigate();
+//   console.log("GameRouter - Current Game State:", gameState);
+//   if (!gameState || !gameState?.type) {
+//     navigate(`/lobby`);
+//     // return <div>در حال بارگذاری اطلاعات بازی...</div>;
+//   }
+
+//   switch (gameState?.type) {
+//     case "feedTheKraken":
+//       return <FeedTheKrakenPage />;
+//     case "mineSweeper":
+//       return <MineSweeperPage />;
+//     default:
+//     // return <div>نوع بازی پشتیبانی نمی‌شود.</div>;
+//   }
+// }
